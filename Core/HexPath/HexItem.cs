2d52e69608212b3287be90512d52a87ad6d2ebd4
Alex Salisbury <alex@alexsalisbury.com>
2021-12-20 08:22:06 -0500

    Build out Hex, HexItem and parameterize svg.

--- Core/HexPath/HexItem.cs
+++ Core/HexPath/HexItem.cs
@@ -1,5 +1,4 @@
 ﻿namespace Dotdev.Core.HexPath
 {
-    public record HexItem (uint Column, uint Row, string Shade);
-
+    public record HexItem (uint Index, uint Column, uint Row, string Shade, string HexClass, string Target, bool Enabled, uint[] Unlocks);
 }
