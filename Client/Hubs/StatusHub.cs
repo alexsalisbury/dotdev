bdd209e8d2e223f395d186fcefed3882358b3971
Alex Salisbury <alex@alexsalisbury.com>
2022-02-23 08:37:52 -0500

    Hub Attempt #1

---
+++ Client/Hubs/StatusHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Dotdev.Client.Hubs;
+
+public class StatusHub : Hub
+{
+    public void BroadcastStatusPacket(int id, DateTimeOffset timestamp)
+    {
+        Clients.All.SendAsync("status", id, timestamp);
+    }
+}
