4bb0b758d212f80e9833bafb7a3750637167fbbe
Alex Salisbury <alex@alexsalisbury.com>
2022-03-09 17:35:39 -0500

    Update ElementFetch to be backed by SqlAzure.

--- Core/Element/Square.cs
+++ Core/Element/Square.cs
@@ -5,9 +5,9 @@
         public void SetLastSeen(DateTimeOffset timestamp) => this.Status = this.Status == null ? null : this.Status with { LastSeen = timestamp }; 
         public ElementInfo Info { get; set; }
         public ServerInfo? Status { get; set; }
-        public uint Column => Info?.Column ?? 0;
-        public uint Row => Info?.Row ?? 0;
-        public string Name => Status?.Name ?? Info?.Name ?? "Unknown";
+        public uint Column => Info?.GridColumn ?? 0;
+        public uint Row => Info?.GridRow ?? 0;
+        public string Name => Status?.Name ?? Info?.ElementName ?? "Unknown";
         public string ServerStatusClass => Status?.Status ?? "unknown";
     }
 }
