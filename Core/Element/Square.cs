215d6d6cf5e3299a845a9ff1b209ee90a523822b
Alex Salisbury <alex@alexsalisbury.com>
2022-03-09 19:47:13 -0500

    Rework ElementInfo

--- Core/Element/Square.cs
+++ Core/Element/Square.cs
@@ -5,8 +5,8 @@
         public void SetLastSeen(DateTimeOffset timestamp) => this.Status = this.Status == null ? null : this.Status with { LastSeen = timestamp }; 
         public ElementInfo Info { get; set; }
         public ServerInfo? Status { get; set; }
-        public uint Column => Info?.GridColumn ?? 0;
-        public uint Row => Info?.GridRow ?? 0;
+        public int Column => Info?.GridColumn ?? 0;
+        public int Row => Info?.GridRow ?? 0;
         public string Name => Status?.Name ?? Info?.ElementName ?? "Unknown";
         public string ServerStatusClass => Status?.Status ?? "unknown";
     }
