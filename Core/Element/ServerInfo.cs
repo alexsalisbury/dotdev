c8046307129dcd612c2f83eee74d979093c19486
Alex Salisbury <alex@alexsalisbury.com>
2022-03-12 14:19:12 -0500

    Start building out LiveStatus a bit.

--- Core/Element/ServerInfo.cs
+++ Core/Element/ServerInfo.cs
@@ -6,10 +6,36 @@
     {
         public uint Number { get; set; }
         public string Symbol { get; set; }
-        public string Status { get; set; }
+        public string LastStatus { get; set; }
         public string Name { get; set; }
         public string IP { get; set; }
         public uint DeviceType { get; set; }
         public DateTimeOffset? LastSeen { get; set; }
+        public string LiveStatus => GetCurrentStatus();
+
+        private string GetCurrentStatus()
+        {
+            return LastStatus switch
+            {
+              //  "online" => "online",
+               // "delayed" => "delayed",
+               // "offline" => "offline",
+                "untracked" => "untracked",
+                _ => GetStatusFromTime(DateTimeOffset.UtcNow)
+            };
+        }
+
+        private string GetStatusFromTime(DateTimeOffset ts)
+        {
+            var delta = ts - LastSeen;
+            return delta?.TotalMinutes switch
+            {
+                null => "unknown",
+                < 6 => "active",
+                < 11 => "delayed",
+                > 11 => "offline",
+                _ => "unknown"
+            };
+        }
     }
 }
