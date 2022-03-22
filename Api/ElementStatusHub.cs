522857bda9a237b99c5ed429fae1c16ece808200
Alex Salisbury <alex@alexsalisbury.com>
2022-03-22 13:37:53 -0400

    Adding Hub to Api as a test.

---
+++ Api/ElementStatusHub.cs
@@ -0,0 +1,27 @@
+﻿namespace Dotdev.Api;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+public class StatusHub : Hub
+{
+    public const string HubUrl = "/elementstatus";
+
+    public void BroadcastStatusPacket(int id, DateTimeOffset timestamp)
+    {
+        Clients.All.SendAsync("elementstatus", new[] { id, timestamp.ToUnixTimeSeconds() });
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        Console.WriteLine($"{Context.ConnectionId} connected");
+        return base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception e)
+    {
+        Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+        await base.OnDisconnectedAsync(e);
+    }
+}
