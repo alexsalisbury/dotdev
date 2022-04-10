167042dff8b1845e5a473bc23bf526d25b0bf072
Alex Salisbury <alex@alexsalisbury.com>
2022-04-10 17:04:53 -0400

    Rework Square a bit for async.

--- Core/Element/Square.cs
+++ Core/Element/Square.cs
@@ -5,13 +5,34 @@
 
         public event EventHandler OnChange;
 
+        public Square() { }
+        public Square(ElementInfo? info, ServerInfo? status)
+        {
+            Info = info;
+            Status = status;
+        }
+
+        public void SetElement(ElementInfo? info)
+        {
+            this.Info  = info ?? this.Info;
+            this.OnChange?.Invoke(this, new EventArgs());
+        }
+
+        public void SetStatus(ServerInfo? status)
+        {
+            this.Status = status ?? this.Status;
+            this.OnChange?.Invoke(this, new EventArgs());
+        }
+
         public void SetLastSeen(DateTimeOffset timestamp)
         {
             this.Status = (this.Status == null ? null : this.Status with { LastSeen = timestamp });
             this.OnChange?.Invoke(this, new EventArgs());
         }
-        public ElementInfo Info { get; set; }
-        public ServerInfo? Status { get; set; }
+
+        public ElementInfo? Info { get; private set; }
+        public ServerInfo? Status { get; private set; }
+        public bool Populated => ((Info is not null) && (Status is not null));
         public int Column => Info?.GridColumn ?? 0;
         public int Row => Info?.GridRow ?? 0;
         public string Name => Status?.Name ?? Info?.ElementName ?? "Unknown";
