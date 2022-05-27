34d0871ea6f9958a31dcf00320c463384a0b6928
Alex Salisbury <alex@alexsalisbury.com>
2022-05-27 13:08:55 -0400

    Core update.

--- Core/HexPath/IHoneycomb.cs
+++ Core/HexPath/IHoneycomb.cs
@@ -4,9 +4,11 @@
 	{
 		event EventHandler<EventArgs> HoneycombChanged;
 		IEnumerable<HexItem> GetItems();
-		bool AddGhosts(HexLocation location);
-		bool EnableGhosts(HexLocation location);
+		ValueTask<bool> AddGhosts(HexLocation location);
+		ValueTask<bool> EnableGhosts(HexLocation location);
 		bool AddRoot(HexLocation location);
+
+		Task UnlockAsync();
 		//bool AddItem(HexItem item);
 		//void Enable(uint row, uint col);
 		//void Enable(HexOrder order);
