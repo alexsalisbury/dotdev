e6efbc052d1c6e4c0e1e18fa9ff902bb467be2cc
Alex Salisbury <alex@alexsalisbury.com>
2022-07-25 11:12:53 -0400

    Function updates. Put data in database for cache.

--- src/HubFunctions/CanaryFunction.cs
+++ src/HubFunctions/CanaryFunction.cs
@@ -1,17 +1,27 @@
 ﻿namespace HubFunctions;
 
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure.Storage.Queues;
 using Microsoft.Azure.WebJobs;
 
 public static class CanaryFunction
 {
+    /// <summary>
+    /// This canary skips past the IoT workflow to help us verify the hub/display end of the pipeline.
+    /// </summary>
+    /// <param name="myTimer"></param>
+    /// <param name="queueClient">Represents a Queue in Azure</param>
+    /// <remarks>Using Copernicium for Canary; Ca (Calcium) was a previous machine name.</remarks>
+    /// <returns>Task</returns>
     [FunctionName("Canary")]
     public static async Task Run(
         [TimerTrigger("0 */5 * * * *")] TimerInfo myTimer,
-        [Queue("status")] IAsyncCollector<StatusDatapoint> notifications)
+        [Queue("elementstatus")] QueueClient queueClient)
     {
-        var result = new StatusDatapoint(1, DateTimeOffset.UtcNow);
-        await notifications.AddAsync(result);
+        var cn = new StatusDatapoint(112, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+        await queueClient.SendMessageAsync(JsonSerializer.Serialize(cn));
+
     }
 }
--- src/HubFunctions/StatusIntake.cs
+++ src/HubFunctions/StatusIntake.cs
@@ -1,28 +1,27 @@
 namespace HubFunctions;
 
-using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-public record StatusDatapoint(uint Id, DateTimeOffset Timestamp);
+public record StatusDatapoint(int Id, long Timestamp);
 
 public static class StatusIntake
 {
     [FunctionName("StatusIntake")]
     public static async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
-        [Queue("status")] IAsyncCollector<StatusDatapoint> notifications,
+        [Queue("elementstatus")] IAsyncCollector<StatusDatapoint> notifications,
         ILogger log)
     {
-            var result = JsonConvert.DeserializeObject<StatusDatapoint>(req.Body.ToString());
-            await notifications.AddAsync(result);
-
+        var data = JsonConvert.DeserializeObject<StatusDatapoint>(req.Body.ToString());
+        await notifications.AddAsync(data);
         return new AcceptedResult();
     }
 }
