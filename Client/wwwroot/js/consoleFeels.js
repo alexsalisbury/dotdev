732cfbd0bb7892a697674a397e95a7abc0230d1d
Alex Salisbury <alex@alexsalisbury.com>
2022-08-04 14:29:17 -0400

    Index and consoleFooter cleanup.

--- Client/wwwroot/js/consoleFeels.js
+++ Client/wwwroot/js/consoleFeels.js
@@ -5,69 +5,20 @@ function stopFeels() {
     clearInterval(feelsTimer);
 }
 
-function consoleQuickPrint(...lines) {
-    clearInterval(feelsTimer);
-    let t = document.getElementById("consoleTarget");
-    let expiry = 1000;
-    let line = 0;
-    let idx = 0;
-
-    function stepAppend() {
-        var text = lines[line];
-        var prev = lines.slice(0, line).join('<br />');
-        var next = idx <= text.length;
-        var sub = text.slice(0, idx);
-        if (next) {
-            idx = idx + 1;
-            console.log(sub);
-            t.innerHTML = prev + '<br />' + sub + '_';
-        } else {
-            if (line < lines.length - 1) {
-                idx = 0;
-                line += 1;
-            } else {
-                clearInterval(feelsTimer);
-            }
-        }
-    }
-
-    feelsTimer = setInterval(stepAppend, 80);
-}
-
-function consoleBlink(expiry = 1000) {
-    clearInterval(feelsTimer);
-    let existing = t.innerHTML;
-
-    function blink() {
-        if (expiry === 0) {
-            clearInterval(feelsTimer);
-        }
-        if (expiry > 0) {
-            var under = (expiry % 2 === 1) ? '_' : ''
-            t.innerHTML = existing + under;
-            console.log(expiry);
-            expiry = expiry - 1;
-        }
-    }
-
-    feelsTimer = setInterval(blink, 80);
-}
-
 function consolePrint(...lines) {
-    let t = document.getElementById("consoleTarget");
-    clearInterval(feelsTimer);
-    let expiry = 1000;
-    let line = 0;
-    let idx = 0;
+        function stepAppend() {
+        let t = document.getElementById("consoleTarget");
+        if (!t) {
+            return;
+        }
 
-    function stepAppend() {
         var text = lines[line];
         var prev = lines.slice(0, line).join('<br />');
-        var next = idx <= text.length;
+        var lineNotDone = idx <= text.length;
         var sub = text.slice(0, idx);
-        if (next) {
+
+        if (lineNotDone) {
             idx = idx + 1;
-            console.log(sub);
             t.innerHTML = prev + '<br />' + sub + '_';
         } else {
             if (line < lines.length - 1) {
@@ -81,20 +32,30 @@ function consolePrint(...lines) {
     }
 
     function blink() {
+        let t = document.getElementById("consoleTarget");
+        if (!t) {
+            expire();
+            return;
+        }
+
         if (expiry === 0) {
             clearInterval(feelsTimer);
         }
+
         if (expiry > 0) {
             var under = (expiry % 2 === 0) ? '_' : ''
             t.innerHTML = lines.join('<br />') + under; // I should probably memoize here.
-            console.log(expiry);
             expiry = expiry - 1;
         }
     }
 
     function expire() {
-        expiry == 0;
+        expiry = 0;
     }
 
+    clearInterval(feelsTimer);
+    let expiry = 1000;
+    let line = 0;
+    let idx = 0;
     feelsTimer = setInterval(stepAppend, 80);
 }
