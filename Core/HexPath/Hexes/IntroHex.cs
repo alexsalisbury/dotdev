2164c41f411a79e651882894bd792d8fc3e6cf54
Alex Salisbury <alex@alexsalisbury.com>
2022-08-04 15:26:50 -0400

    Hex DefaultText. Cleanup.

--- Core/HexPath/Hexes/IntroHex.cs
+++ Core/HexPath/Hexes/IntroHex.cs
@@ -2,7 +2,7 @@
 {
     public record IntroHex : HexItem
     {
-        public IntroHex(HexLocation location) : base(location, DefaultStyle)
+        public IntroHex(HexLocation location) : base(location, DefaultStyle, DefaultText)
             //: base(HexOrder.Intro, column, row, "#A1AEFF", "hexOne", "./hex_placeholder.png", true, (5, HexOrder.About))
         {
             this.Location = location;
@@ -15,5 +15,7 @@
 
 
         public static (uint, uint) DefaultLocation => (2,5);
+
+        public static string[] DefaultText = { "Welcome...initializing...", "1/3 modules loaded", "[This space intentionally left minimalist]", ">" };
     }
 }
