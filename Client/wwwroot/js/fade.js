e08744eb39cf608b97d886823f2b9cc137a3eef0
Alex Salisbury <alex@alexsalisbury.com>
2022-09-21 18:50:16 -0400

    Doing some dynamic background fade. <3

---
+++ Client/wwwroot/js/fade.js
@@ -0,0 +1,10 @@
+﻿function screenToSVG(screenX, screenY) {
+	const svg = document.getElementById("svgFade");
+	var p = svg.createSVGPoint();
+	p.x = screenX;
+	p.y = screenY;
+	var ctx = svg.getScreenCTM().inverse();
+	var k = p.matrixTransform(ctx);
+	var res = k.x + "_" + k.y;
+	return res;
+}
