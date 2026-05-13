4bce717bd3a61011b3a5576ad369b6243f95ab80
Alex Salisbury <alex@alexsalisbury.com>
2026-05-13 08:12:18 -0500

    Allow GET and POST on SignalR negotiate endpoint

--- HubFunctions/ElementNegotiate.cs
+++ HubFunctions/ElementNegotiate.cs
@@ -7,7 +7,7 @@ public static class HubNegotiateFunction
 {
     [Function("negotiate")]
     public static SignalRConnectionInfo Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
         [SignalRConnectionInfoInput(HubName = "dotdev")] SignalRConnectionInfo connectionInfo)
     {
         return connectionInfo;
